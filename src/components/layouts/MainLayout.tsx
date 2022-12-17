9c6f50fdf3037d60dee0425fb4decbcb1c6614ae
vildivent <vildivent@mail.ru>
2022-12-18 02:47:58 +0300

    feat: scoll to top, mountains

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,6 +3,8 @@ import Footer from "../Footer";
 import Image, { type StaticImageData } from "next/image";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
+import ScrollToTop from "react-scroll-to-top";
+import { IoIosArrowUp } from "react-icons/io";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -41,18 +43,34 @@ const MainLayout = ({ children, imgSrc, alt, title }: MainLayoutProps) => {
 
         <div className="absolute z-10 mt-[55vh] flex w-full flex-col lg:mt-[70vh] xl:mt-[80vh] 2xl:mt-[80vh]">
           <div
-            className="flex h-[35vh] items-center justify-center bg-blue-3 px-5"
+            className="relative flex h-[35vh] items-center justify-center bg-blue-3 px-5"
             style={{
               clipPath: "polygon(0 0, 0 86%, 100% 100%, 100% 29%)",
             }}
           >
-            <h1 className=" text-center font-h text-3xl font-bold text-white xl:text-4xl 2xl:text-5xl">
+            <h1 className="relative z-[1] text-center font-h text-3xl font-bold text-white xl:text-4xl 2xl:text-5xl">
               {title}
             </h1>
+            <div
+              className="absolute bottom-0 z-0 h-[35vw] w-full bg-white sm:h-[20vw]"
+              style={{
+                clipPath:
+                  "polygon(100% 100%, 85% 65%, 73% 85%, 65% 70%, 50% 100%)",
+              }}
+            />
             <div className="w-[15%]" />
           </div>
           {children}
           <Footer />
+          <ScrollToTop
+            style={{ backgroundColor: "#cccccc" }}
+            component={
+              <span className="flex items-center justify-center text-3xl">
+                <IoIosArrowUp />
+              </span>
+            }
+            smooth
+          />
         </div>
       </main>
     </>
